6015866ac12ac53a06cb8ee06e6a0351308059c9
cindyhont <cindyhont@gmail.com>
2022-11-16 18:20:02 +0000

    .

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.7
 	github.com/rs/cors v1.8.2
-	golang.org/x/crypto v0.1.0
+	golang.org/x/crypto v0.2.0
 	golang.org/x/oauth2 v0.1.0
 )
 
@@ -19,7 +19,8 @@ require (
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
-	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
@@ -27,5 +28,5 @@ require (
 require (
 	github.com/gobwas/ws v1.1.0
 	github.com/google/uuid v1.3.0
-	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 )
--- router/main.go
+++ router/main.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
 
@@ -9,12 +10,67 @@ import (
 )
 
 var Router *httprouter.Router
+var HttpRouter *httprouter.Router
 
 func init() {
 	Router = httprouter.New()
+	HttpRouter = httprouter.New()
 }
 
+func testHttpsAPI(
+	w http.ResponseWriter,
+	r *http.Request,
+	p httprouter.Params,
+) {
+	json.NewEncoder(w).Encode(map[string]bool{"https-success": true})
+}
+
+/*
+func testHttpAPI(
+
+	w http.ResponseWriter,
+	r *http.Request,
+	p httprouter.Params,
+
+	) {
+		json.NewEncoder(w).Encode(map[string]bool{"http-success": true})
+	}
+*/
+
 func Listen() {
+	Router.GET("/", testHttpsAPI)
 	handler := cors.Default().Handler(Router)
 	http.ListenAndServe(":"+os.Getenv("PORT"), handler)
 }
+
+/*
+func Listen() {
+	// http
+	HttpRouter.GET("/testhttpapi", testHttpAPI)
+	httpHandler := cors.Default().Handler(HttpRouter)
+	go http.ListenAndServe(":"+os.Getenv("PORT"), httpHandler)
+
+	// https
+	// always !!! => port 80 for http, port 443 for https
+	Router.GET("/testhttpsapi", testHttpsAPI)
+	httpsHandler := cors.Default().Handler(Router)
+
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		Cache:      autocert.DirCache("certs"),
+		Email:      os.Getenv("EMAIL_ADDRESS"),
+		HostPolicy: autocert.HostWhitelist(os.Getenv("DOMAIN_NAME")),
+	}
+
+	server := &http.Server{
+		Addr:    ":443",
+		Handler: httpsHandler,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+
+	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
+	log.Panic(server.ListenAndServeTLS("", ""))
+}
+*/
