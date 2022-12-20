d2acab55d55d69b1e77e33616ba16550dd1aa8ad
cindyhont <cindyhont@gmail.com>
2022-12-20 16:32:19 +0000

    .

--- instantcomm/variables.go
+++ instantcomm/variables.go
@@ -2,16 +2,15 @@ package instantcomm
 
 import (
 	"net"
-	"time"
 
 	"github.com/streadway/amqp"
 )
 
 var (
-	wsUsers              = map[string]map[*net.Conn]bool{}
-	servers              = map[string]time.Time{}
-	otherServersConn     = map[string]map[string]int{}
-	rabbitmqChannel      *amqp.Channel
-	messageQueue         amqp.Queue
+	wsUsers = map[string]map[*net.Conn]bool{}
+	// servers              = map[string]time.Time{}
+	otherServersConn = map[string]map[string]int{}
+	rabbitmqChannel  *amqp.Channel
+	// messageQueue         amqp.Queue
 	serverHeartbeatQueue amqp.Queue
 )
